a9fe8f7bf37084af6eeb51f3d7e5d203f90aa954
dylanrubini <dylan.rubini@hotmai.co.uk>
2024-08-29 18:00:14 +0100

    Close port if open

--- run_locally.sh
+++ run_locally.sh
@@ -2,9 +2,19 @@
 micromamba deactivate 
 micromamba activate rosic-site
 
-URL="http://localhost:4001/"
+PORT="4001"
+URL="http://localhost:$PORT/"
 
-jekyll serve --port 4001 &
+# Check if the port is already in use and kill the process using it
+if lsof -i:$PORT -t >/dev/null 2>&1; then
+    echo "Port $PORT is in use. Killing the process using it..."
+    kill -9 $(lsof -i:$PORT -t)
+    echo "Process on port $PORT has been terminated."
+else
+    echo "Port $PORT is not in use."
+fi
+
+jekyll serve --port $PORT &
 
 sleep 3 
 
